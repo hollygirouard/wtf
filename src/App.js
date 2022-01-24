675811a89cb78653c42cbacee13caa1197394b65
hollygirouard <holly@hoopsbyholly.com>
2022-01-24 09:43:15 -0700

    example for tim

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from 'react'
+import { Navbar, NavbarBrand, NavbarToggler, Nav, NavItem, Collapse, NavLink } from 'reactstrap'
 import { Route, Routes, Link } from 'react-router-dom'
 import NestedComponent from './components/NestedComponent'
-import './App.css';
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const App = () => {
   const [apiData, setApiData] = useState([])
-  const url =
-    'https://xeno-canto.org/api/2/recordings?query=cnt:brazil'
+  const [ navExpand, setNavExpand ] = useState(false)
+  const [ loading, setLoading ] = useState(false)
+  // const url =
+  //   'https://xeno-canto.org/api/2/recordings?query=cnt:brazil'
 
   // useEffect(() => {
   //   fetch(url)
@@ -16,36 +20,58 @@ const App = () => {
   //     })
   // }, [apiData])
 
-  let headers = new Headers()
-  headers.append('Content-Type', 'application/json')
-  headers.append('Accept', 'application/json')
-  headers.append('Origin', 'http://localhost:3000')
+  // let headers = new Headers()
+  // headers.append('Content-Type', 'application/json')
+  // headers.append('Accept', 'application/json')
+  // headers.append('Origin', 'http://localhost:3000')
   
-    useEffect(() => {
-        fetch(url, {
-          headers: headers,
-          mode: 'no-cors'
-         })
-          .then((res) => res.json())
-          .then(json => {
-            setApiData(json)
-            console.log(apiData)
-          })
-          .catch(console.error)
-      },
-      [headers, apiData]
-    )
+  //   useEffect(() => {
+  //       fetch(url,
+  //       //   {
+  //       //   headers: headers,
+  //       //   mode: 'no-cors'
+  //       //  }
+  //        )
+  //         .then((res) => res.json())
+  //         .then(json => {
+  //           setApiData(json)
+  //           console.log(apiData)
+  //         })
+  //         .catch(console.error)
+  //     },
+  //     [ apiData ]
+  //   )
   
-  console.log(apiData)
+  // console.log(apiData)
+  console.log(navExpand)
 
-  if (!apiData) {
+  if (loading) {
     <p>Site Loading</p>
   }
 
   return (
-    <div><nav><Link to='/'>Home</Link></nav>
+    <div>
+      <Navbar color='faded' light>
+        <NavbarBrand className='me-auto'>Demo Page</NavbarBrand>
+        <NavbarToggler
+          className='me-2'
+          onClick={() => setNavExpand(!navExpand)}
+        />
+        <Collapse navbar isOpen={ navExpand }>
+          <Nav navbar>
+            <NavItem>
+              <NavLink href='#'>Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href='https://github.com/reactstrap/reactstrap'>
+                GitHub
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
       <Routes>
-        <Route path='/' element={ <NestedComponent nestedData={ apiData } />} />
+        <Route path='/' element={<NestedComponent nestedData={apiData} />} />
       </Routes>
     </div>
   )
--- src/components/NestedComponent.js
+++ src/components/NestedComponent.js
@@ -5,14 +5,14 @@ const NestedComponent = ({ nestedData  }) => {
   return (
     <div>
       <h1>Nested Data</h1>
-      {/* {
+      {
         nestedData.map((data) => (
           <>
           <p>ID: { data.id } </p>
           <p>Type: { data.type }</p>
           </>
         ))
-      } */}
+      }
     </div>
   )
 }
